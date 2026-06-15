ef0d7f6c524b771592c2e60beda62f3a6c9ec559
GiovanneHMSA <ginhodime@gmail.com>
2026-06-15 12:41:53 -0300

    deu certo!

---
+++ js/scripit.js
@@ -0,0 +1,9 @@
+function calcular(){
+let num1 = document.getElementById('edtnum1').value;
+let num2 = document.getElementById('edtnum2').value;
+
+let sub = num1 - num2;
+console.log('subtração ='+sub);
+let mult = num1 * num2;
+console.log('multiplicação ='+mult);
+}
